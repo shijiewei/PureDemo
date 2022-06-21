1796ca150232bd1ae11aa17e783f0717b7f9cdd6
shijiewei <jackiewayne2046@163.com>
2022-06-21 16:19:27 +0800

    init

---
+++ app/src/main/java/com/mob/puredemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mob.puredemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
